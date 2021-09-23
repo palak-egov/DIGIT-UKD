974d5a303b91a933b77bfaed9c4981c6c6ecefe0
harit-egov <harit.saxena@egovernments.org>
2021-09-23 16:37:47 +0530

    initial setup

---
+++ frontend/micro-ui/web/src/customizations/index.js
@@ -0,0 +1,7 @@
+import SelectTradeLicenceCustomization from "./TL/selectTrade";
+import SelectOccupanCyType from "./TL/SelectOccupancyType";
+
+export const customizations = [
+  SelectTradeLicenceCustomization,
+  SelectOccupanCyType,
+];
