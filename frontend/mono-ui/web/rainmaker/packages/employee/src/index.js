2b89f9388b7e7585eb4fd1fd367a96c438c96d3e
Prasad Reddy <guruprasadreddy.narala@egovernments.org>
2021-09-29 16:56:08 +0530

    Testing Employee url fix

--- frontend/mono-ui/web/rainmaker/packages/employee/src/index.js
+++ frontend/mono-ui/web/rainmaker/packages/employee/src/index.js
@@ -36,7 +36,13 @@ WebFont.load({
 });
 
 // move it to a env file
-window.basename = process.env.NODE_ENV === "production" ? "/employee" : "";
+if (window.location.href && window.location.href.includes('digit-ui')) {
+  window.basename = process.env.NODE_ENV === "production" ? "/" : "";
+}
+else
+{
+   window.basename = process.env.NODE_ENV === "production" ? "/employee" : "";
+}
 window.__CURRENT_APP__ = "EMPLOYEE"
 // hardcoded the base; to be changed soon!!!!!
 render(
