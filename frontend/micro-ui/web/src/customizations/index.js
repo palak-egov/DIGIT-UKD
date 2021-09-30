636b057a76a810b692291c8d698aa17a10c1a1bc
harit-egov <harit.saxena@egovernments.org>
2021-09-30 10:56:00 +0530

    correction

--- frontend/micro-ui/web/src/customizations/index.js
+++ frontend/micro-ui/web/src/customizations/index.js
@@ -1,11 +1,10 @@
 import SelectTradeLicenceCustomization from "./TL/selectTrade";
 import SelectOccupanCyType from "./TL/SelectOccupancyType";
-import SelectProofIdentity from "./TL/proof";
+// import SelectProofIdentity from "./TL/proof";
 import SelectOwnerShipDetails from "./TL/SelectOwnerShipDetails";
 
 export const customizations = [
   SelectTradeLicenceCustomization,
   SelectOccupanCyType,
-  SelectProofIdentity,
   SelectOwnerShipDetails,
 ];
