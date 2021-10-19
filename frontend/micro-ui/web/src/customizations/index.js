db82e3b91ac31351b2d7c421ec77554f641138de
harit-egov <harit.saxena@egovernments.org>
2021-10-19 23:04:27 +0530

    commented hrms section

--- frontend/micro-ui/web/src/customizations/index.js
+++ frontend/micro-ui/web/src/customizations/index.js
@@ -2,7 +2,7 @@ import SelectTradeLicenceCustomization from "./TL/selectTrade";
 import SelectOccupanCyType from "./TL/SelectOccupancyType";
 // import SelectProofIdentity from "./TL/proof";
 import SelectOwnerShipDetails from "./TL/SelectOwnerShipDetails";
-import CreateEmployee from "./HRMS/CreateEmployee";
+// import CreateEmployee from "./HRMS/CreateEmployee";
 import SelectOwnerDetails from "./TL/SelectOwnerDetails";
 import TlAcknowledgement from "./TL/TlAcknowledgement";
 import TLGSTNumber from "./TL/TLGSTNumber"
