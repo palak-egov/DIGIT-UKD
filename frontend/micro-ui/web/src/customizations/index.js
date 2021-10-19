d128a6446bb49131f2e34bd198f582a0e9e97cdc
Prasad Reddy <guruprasadreddy.narala@egovernments.org>
2021-10-19 15:40:55 +0530

    Two Fields added No of Employees, Operational Area

--- frontend/micro-ui/web/src/customizations/index.js
+++ frontend/micro-ui/web/src/customizations/index.js
@@ -2,10 +2,13 @@ import SelectTradeLicenceCustomization from "./TL/selectTrade";
 import SelectOccupanCyType from "./TL/SelectOccupancyType";
 // import SelectProofIdentity from "./TL/proof";
 import SelectOwnerShipDetails from "./TL/SelectOwnerShipDetails";
+import CreateEmployee from "./HRMS/CreateEmployee";
 import SelectOwnerDetails from "./TL/SelectOwnerDetails";
 import TlAcknowledgement from "./TL/TlAcknowledgement";
 import TLGSTNumber from "./TL/TLGSTNumber"
 import SelectTradeUnits from "./TL/SelectTradeUnits";
+import TLNoOfEmployees from "./TL/TLNoOfEmployees";
+import TLOperationalArea from "./TL/TLOperationalArea";
 
 export const customizations = [
   SelectTradeLicenceCustomization,
@@ -15,4 +18,6 @@ export const customizations = [
   TlAcknowledgement,
   TLGSTNumber,
   SelectTradeUnits,
+  TLOperationalArea,
+  TLNoOfEmployees
 ];
