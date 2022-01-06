ba3b75ed5ea8c8b12b83f7e64c50c7563d938d12
Prasad Reddy <guruprasadreddy.narala@egovernments.org>
2022-01-06 17:59:51 +0530

    accepting hindi names - Employee

--- frontend/micro-ui/web/src/customizations/TL/TLTradeDetailsEmployee.js
+++ frontend/micro-ui/web/src/customizations/TL/TLTradeDetailsEmployee.js
@@ -357,7 +357,7 @@ const OwnerForm1 = (_props) => {
                 control={control}
                 name={"tradeName"}
                 defaultValue={tradedetail?.tradeName}
-                rules={{ required: t("REQUIRED_FIELD"), validate: { pattern: (val) => (/^[-@.\/#&+\w\s]*$/.test(val) ? true : t("INVALID_NAME")) } }}
+                rules={{ required: t("REQUIRED_FIELD"), validate: { pattern: (val) => (/^[^{0-9}^\$\"'<>?\\\\~`!@#$%^()+={}\[\]*,._:;“”‘’]*$/.test(val) ? true : t("INVALID_NAME")) } }}
                 render={(props) => (
                   <TextInput
                     value={props.value}
