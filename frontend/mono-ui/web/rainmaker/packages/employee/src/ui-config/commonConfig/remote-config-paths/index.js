f4cb85866684c2a0ff89d8a09a42186d01575a28
Saumya-egov <74046603+saumya-egov@users.noreply.github.com>
2022-01-24 12:38:51 +0530

    adding bill-amend module path

--- frontend/mono-ui/web/rainmaker/packages/employee/src/ui-config/commonConfig/remote-config-paths/index.js
+++ frontend/mono-ui/web/rainmaker/packages/employee/src/ui-config/commonConfig/remote-config-paths/index.js
@@ -32,6 +32,9 @@ const remoteConfigPath = (path, screenKey) => {
     case "egov-common":
       config = require(`egov-common/ui-config/screens/specs/${path}/${screenKey}`).default;
       break;
+    case "bill-amend":
+      config = require(`egov-billamend/ui-config/screens/specs/${path}/${screenKey}`).default;
+      break;
     // case "egov-boilerplate":
     //   config = require(`egov-boilerplate/ui-config/screens/specs/${path}/${screenKey}`).default;
     //   break;
