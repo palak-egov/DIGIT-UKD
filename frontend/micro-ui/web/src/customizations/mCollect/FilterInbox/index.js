a43d406ef1683bd76b5efbfb9a23ca5b6fa7b1bd
Ayush Agarwal <Ayushagarwal@trisysit.com>
2022-02-03 17:07:17 +0530

    changes for the filter mcs for locality search

--- frontend/micro-ui/web/src/customizations/mCollect/FilterInbox/index.js
+++ frontend/micro-ui/web/src/customizations/mCollect/FilterInbox/index.js
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
-import { CloseSvg, Localities, RemoveableTag, SubmitBar } from "@egovernments/digit-ui-react-components";
+import {
+  CloseSvg,
+  Localities,
+  RemoveableTag,
+  SubmitBar,
+} from "@egovernments/digit-ui-react-components";
 
 import { useTranslation } from "react-i18next";
-import Status  from "../Status";
+import Status from "../Status";
 import ServiceCategory from "../ServiceCategory";
 import _ from "lodash";
- 
-const FilterInbox = ({ searchParams, onFilterChange, defaultSearchParams, ...props }) => {
+
+const FilterInbox = ({
+  searchParams,
+  onFilterChange,
+  defaultSearchParams,
+  ...props
+}) => {
   const { t } = useTranslation();
   const tenantId = window.Digit.ULBService.getCurrentTenantId();
   const [_searchParams, setSearchParams] = useState(() => searchParams);
-  const [ clearCheck, setclearCheck] = useState(false);
+  const [clearCheck, setclearCheck] = useState(false);
+  const [searchLocality, setSearchLocality] = useState([]);
+  const [locality, setlocality] = useState([]);
 
   const localParamChange = (filterParam) => {
     setclearCheck(false);
@@ -18,6 +30,7 @@ const FilterInbox = ({ searchParams, onFilterChange, defaultSearchParams, ...pro
     let _new = { ..._searchParams, ...filterParam };
     if (keys_to_delete) keys_to_delete.forEach((key) => delete _new[key]);
     delete filterParam.delete;
+    console.log("ayush 1", _new);
     setSearchParams({ ..._new });
   };
 
@@ -27,8 +40,25 @@ const FilterInbox = ({ searchParams, onFilterChange, defaultSearchParams, ...pro
     setclearCheck(true);
   };
 
-  const selectLocality = (d) => {
-    localParamChange({ locality: [ ...(_searchParams?.locality || []) , d ] });
+  const selectLocality = (d, remove = false) => {
+    console.log("ayush", d, searchLocality, locality);
+    if (remove) {
+      setSearchLocality(
+        searchLocality.filter((value) => {
+          return value.code !== d.code;
+        })
+      );
+      let local = locality.filter((value) => {
+        return value !== d.code;
+      });
+      setlocality(local);
+      localParamChange({ locality: local.join(",") });
+    } else {
+      let local = [...locality, d.code];
+      setSearchLocality([...searchLocality, d]);
+      setlocality(local);
+      localParamChange({ locality: local.join(",") });
+    }
   };
 
   return (
@@ -36,23 +66,44 @@ const FilterInbox = ({ searchParams, onFilterChange, defaultSearchParams, ...pro
       <div className="filter">
         <div className="filter-card">
           <div className="heading" style={{ alignItems: "center" }}>
-            <div className="filter-label" style={{ display: "flex", alignItems: "center" }}>
+            <div
+              className="filter-label"
+              style={{ display: "flex", alignItems: "center" }}
+            >
               <span>
-                <svg width="17" height="17" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 22 22"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path
                     d="M0.66666 2.48016C3.35999 5.9335 8.33333 12.3335 8.33333 12.3335V20.3335C8.33333 21.0668 8.93333 21.6668 9.66666 21.6668H12.3333C13.0667 21.6668 13.6667 21.0668 13.6667 20.3335V12.3335C13.6667 12.3335 18.6267 5.9335 21.32 2.48016C22 1.60016 21.3733 0.333496 20.2667 0.333496H1.71999C0.613327 0.333496 -0.01334 1.60016 0.66666 2.48016Z"
                     fill="#505A5F"
                   />
                 </svg>
               </span>
-              <span style={{ marginLeft: "8px", fontWeight: "normal" }}>{t("UC_FILTERS_LABEL")}:</span>
+              <span style={{ marginLeft: "8px", fontWeight: "normal" }}>
+                {t("UC_FILTERS_LABEL")}:
+              </span>
             </div>
             <div className="clearAll" onClick={clearAll}>
               {t("ES_COMMON_CLEAR_ALL")}
             </div>
             {props.type === "desktop" && (
-              <span className="clear-search" onClick={clearAll} style={{ border: "1px solid #e0e0e0", padding: "6px" }}>
-                <svg width="17" height="17" viewBox="0 0 16 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <span
+                className="clear-search"
+                onClick={clearAll}
+                style={{ border: "1px solid #e0e0e0", padding: "6px" }}
+              >
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 16 22"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path
                     d="M8 5V8L12 4L8 0V3C3.58 3 0 6.58 0 11C0 12.57 0.46 14.03 1.24 15.26L2.7 13.8C2.25 12.97 2 12.01 2 11C2 7.69 4.69 5 8 5ZM14.76 6.74L13.3 8.2C13.74 9.04 14 9.99 14 11C14 14.31 11.31 17 8 17V14L4 18L8 22V19C12.42 19 16 15.42 16 11C16 9.43 15.54 7.97 14.76 6.74Z"
                     fill="#505A5F"
@@ -75,8 +126,16 @@ const FilterInbox = ({ searchParams, onFilterChange, defaultSearchParams, ...pro
                 clearCheck={clearCheck}
                 setclearCheck={setclearCheck}
                 onAssignmentChange={(e, status) => {
-                  if (e.target.checked) localParamChange({ status: [..._searchParams?.status, status?.code] });
-                  else localParamChange({ status: _searchParams?.status.filter((e) => e !== status?.code) });
+                  if (e.target.checked)
+                    localParamChange({
+                      status: [..._searchParams?.status, status?.code],
+                    });
+                  else
+                    localParamChange({
+                      status: _searchParams?.status.filter(
+                        (e) => e !== status?.code
+                      ),
+                    });
                 }}
               />
             </div>
@@ -84,16 +143,20 @@ const FilterInbox = ({ searchParams, onFilterChange, defaultSearchParams, ...pro
               <div className="filter-label" style={{ fontWeight: "normal" }}>
                 {t("ES_INBOX_LOCALITY")}:
               </div>
-              <Localities selectLocality={selectLocality} tenantId={tenantId} boundaryType="revenue" />
+              <Localities
+                selectLocality={selectLocality}
+                tenantId={tenantId}
+                boundaryType="revenue"
+              />
               <div className="tag-container">
-                {_searchParams?.locality?.map((locality, index) => {
+                {searchLocality?.map((locality, index) => {
                   return (
                     <RemoveableTag
                       key={index}
                       // text={locality.name}
                       text={t(`${locality.i18nkey}`)}
                       onClick={() => {
-                        localParamChange({ locality: _searchParams?.locality.filter((loc) => loc.code !== locality.code) });
+                        selectLocality(locality, true);
                       }}
                     />
                   );
@@ -107,8 +170,19 @@ const FilterInbox = ({ searchParams, onFilterChange, defaultSearchParams, ...pro
                 businessServices={_searchParams.services}
                 clearCheck={clearCheck}
                 onAssignmentChange={(e, businessService) => {
-                  if (e.target.checked) localParamChange({ businessService: [..._searchParams?.businessService, businessService?.code] });
-                  else localParamChange({ businessService: _searchParams?.businessService.filter((e) => e !== businessService?.code) });
+                  if (e.target.checked)
+                    localParamChange({
+                      businessService: [
+                        ..._searchParams?.businessService,
+                        businessService?.code,
+                      ],
+                    });
+                  else
+                    localParamChange({
+                      businessService: _searchParams?.businessService.filter(
+                        (e) => e !== businessService?.code
+                      ),
+                    });
                 }}
               />
             </div>
@@ -128,10 +202,10 @@ const FilterInbox = ({ searchParams, onFilterChange, defaultSearchParams, ...pro
 };
 
 const customize = () => {
-    window.Digit.ComponentRegistryService.setComponent(
-        "MCOLLECT_INBOX_FILTER",
-        FilterInbox
-      );
+  window.Digit.ComponentRegistryService.setComponent(
+    "MCOLLECT_INBOX_FILTER",
+    FilterInbox
+  );
 };
 
 export default customize;
